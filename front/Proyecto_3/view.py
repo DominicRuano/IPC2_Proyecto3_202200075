c6a336dc6fd01db31282f54a9cea015118e13d46
DominicRuano <Dojuparupe1975@gmail.com>
2023-11-02 13:34:24 -0600

    se envian los archivos desde el front a el back

--- front/Proyecto_3/view.py
+++ front/Proyecto_3/view.py
@@ -1,10 +1,12 @@
 from django.http import HttpResponse
 from django.template import loader
 from django.shortcuts import redirect
+from django.views.decorators.csrf import csrf_exempt
 from requests import post, get
 import requests
 
 seInicializo = False
+seCargoMensajes = False
 
 def getInfo(request):
     return HttpResponse("Frontend: Estudiantes de ipc2")
@@ -66,14 +68,77 @@ def buttonReset(request):
         return redirect("http://127.0.0.1:8000/reset/")
 
 def cargarMensajes(request):
+    global seCargoMensajes
     objHTML = loader.get_template("cargarMensajes.html")
-    html = objHTML.render()
+    vacio = seCargoMensajes
+    seCargoMensajes = False
+    html = objHTML.render({"estaVacio": vacio})
     return HttpResponse(html)
 
+@csrf_exempt
+def buttonMensaje(request):
+    global seCargoMensajes
+    try:
+            api_url = 'http://127.0.0.1:3050/grabarMensajes'
+
+            # Verifica si se ha seleccionado un archivo
+            if 'archivo' in request.FILES:
+                archivo = request.FILES['archivo']
+
+                # Configura la solicitud POST con el archivo adjunto
+                files = {'archivo': archivo}
+
+                # Realiza la solicitud POST a la API
+                response = requests.post(api_url, files=files)
+                if response.status_code == 200:
+                    # La solicitud se realizó con éxito
+                    seCargoMensajes = True
+                    return redirect("http://127.0.0.1:8000/cargarMensajes/")
+                else:
+                    # La solicitud falló
+                    return redirect("http://127.0.0.1:8000/cargarMensajes/")
+
+            # Si no se seleccionó un archivo o hubo un error
+            return redirect("http://127.0.0.1:8000/cargarMensajes/")
+
+    except Exception as e:
+        return HttpResponse(f'Error al enviar el archivo a la API: {str(e)}')
+
 def cargarConfiguraciones(request):
+    global seCargoMensajes
     objHTML = loader.get_template("cargarConfiguracion.html")
-    html = objHTML.render()
+    html = objHTML.render({"estaVacio": seCargoMensajes })
+    seCargoMensajes = False
     return HttpResponse(html)
+
+@csrf_exempt
+def buttonconfig(request):
+    global seCargoMensajes
+    try:
+            api_url = 'http://127.0.0.1:3050/grabarPalabras'
+
+            # Verifica si se ha seleccionado un archivo
+            if 'archivo' in request.FILES:
+                archivo = request.FILES['archivo']
+
+                # Configura la solicitud POST con el archivo adjunto
+                files = {'archivo': archivo}
+
+                # Realiza la solicitud POST a la API
+                response = requests.post(api_url, files=files)
+                if response.status_code == 200:
+                    # La solicitud se realizó con éxito
+                    seCargoMensajes = True
+                    return redirect("http://127.0.0.1:8000/cargarConfiguraciones/")
+                else:
+                    # La solicitud falló
+                    return redirect("http://127.0.0.1:8000/cargarConfiguraciones/")
+
+            # Si no se seleccionó un archivo o hubo un error
+            return redirect("http://127.0.0.1:8000/cargarConfiguraciones/")
+
+    except Exception as e:
+        return HttpResponse(f'Error al enviar el archivo a la API: {str(e)}')
 
 def consultarHastags(request):
     objHTML = loader.get_template("consultarHastags.html")
